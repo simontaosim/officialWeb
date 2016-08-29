f4d3c543cb2770289ec450e0b837264534717916
simontaosim <xsqfeather@126.com>
2016-08-29 16:24:36 +0800

    modified:   package.json 	modified:   src/components/MainNav/MainNav.css 	modified:   src/components/OnePageSwitcher/OnePageSwitcher.css 	modified:   src/components/OnePageSwitcher/OnePageSwitcher.js 	modified:   src/entries/index.html

--- src/components/OnePageSwitcher/OnePageSwitcher.js
+++ src/components/OnePageSwitcher/OnePageSwitcher.js
@@ -1,47 +1,34 @@
 'use strict';
 
 import React from 'react';
+
+import { Carousel, Button } from 'antd';
 import './OnePageSwitcher.css'
-import {SectionsContainer, Section, Header, Footer} from 'react-fullpage';
 
 
 class OnePageSwitcher extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  handleScroll(){
+    console.log("scroll happened");
+  }
   render() {
-    let options = {
-        activeClass:          'active', // the class that is appended to the sections links
-        anchors:              [], // the anchors for each sections
-        arrowNavigation:      true, // use arrow keys
-        className:            'SectionContainer', // the class name for the section container
-        delay:                1000, // the scroll animation speed
-        navigation:           true, // use dots navigatio
-        scrollBar:            false, // use the browser default scrollbar
-        sectionClassName:     'Section', // the section class name
-        sectionPaddingTop:    '0', // the section top padding
-        sectionPaddingBottom: '0', // the section bottom padding
-        verticalAlign:        false // align the content of each section vertical
-      };
 
     return (
-      <div>
-      <Header>
-          <a href="#sectionOne" className="opa">Section One</a>
-          <a href="#sectionTwo">Section Two</a>
-          <a href="#sectionThree">Section Three</a>
-        </Header>
-        <Footer>
-          <a href="" className="opa">Dcoumentation</a>
-          <a href="">Example Source</a>
-          <a href="">About</a>
-        </Footer>
-        <SectionsContainer {...options}>
-          <Section>Page 1</Section>
-          <Section>Page 2</Section>
-          <Section>Page 3</Section>
-        </SectionsContainer>
+      <div onScroll={this.handleScroll.bind(this)} style={{width: "100%", height: "100%", textAlign: "center"}}>
+        <Carousel effect="fade" vertical={true}  onScroll={this.handleScroll.bind(this)}
+         useCSS={true}
+        dots={true}
+        >
+            <div style={{background: "blue", width: "100%"}}><h3>第一页</h3></div>
+            <div style={{background: "red", width: "100%"}}><h3>第二页</h3></div>
+            <div style={{background: "yellow", width: "100%"}}><h3>第三页</h3></div>
+            <div style={{background: "grey", width: "100%"}}><h3>第四页</h3></div>
+        </Carousel>
       </div>
+
     );
   }
 }
